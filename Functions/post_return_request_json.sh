abc695d9ed8ea658e8a216db88f78adabba28776
TomBu <40271216+bucto@users.noreply.github.com>
2025-11-10 14:30:40 +0100

    Update post_return_request_json.sh

--- Functions/post_return_request_json.sh
+++ Functions/post_return_request_json.sh
@@ -9,7 +9,7 @@ post_return_request_json() {
 
   TEMP_JSON_PICKUPRESULT="$WORK_FOLDER/JSON_RESPONSE/$x.json"
 
-  curl -s -X POST "https://onlinetools.ups.com/api/pickupcreation/v1/pickups" \
+  curl -s -X POST "$UPS_PICKUP_URL" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $ACCESS_TOKEN" \
     -d @"$TEMP_JSON_PICKUPREQUEST" \
@@ -17,4 +17,5 @@ post_return_request_json() {
 
   echo "UPS response saved: $TEMP_JSON_PICKUPRESULT"
 }
+
 
